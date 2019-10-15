7bab194c0681c2338498d98b2707551b54951477
Aliaksandr Shyrokau <aliaksandrs@google.com>
2019-10-15 14:30:22 +0300

    added result.py file which make requests to thread and process and build a  histgram out of the data

---
+++ result.py
@@ -0,0 +1,40 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import requests
+import json
+
+
+r_t = requests.get('http://0.0.0.0:1010/data')
+r_m = requests.get('http://0.0.0.0:3010/data')
+
+
+result_t = json.loads(r_t.content)
+result_m = json.loads(r_m.content)
+
+# plt.style.use('fivethirtyeight')
+# comic style...
+plt.xkcd()
+
+type = ['threading', 'multiprocessing']
+
+x_indexes = np.arange(len(type))
+width = 0.2
+
+resize = [result_t['results'][0]['time_to_resize'], result_m['results'][0]['time_to_resize']]
+download = [result_t['results'][0]['time_to_download'], result_m['results'][0]['time_to_download']]
+
+
+plt.bar(x_indexes + width, resize, width=width, color='#00ffff', label='Resize')
+plt.bar(x_indexes, download, width=width,  color='#9f9fff', label='Download')
+
+
+plt.xticks(ticks=x_indexes, labels=type)
+
+plt.xlabel('downloading and resizing ' +
+           str(result_m['results'][0]['number_of_images']) + ' image(s)...')
+plt.ylabel('Time(seconds)')
+plt.title('Threading vs MultiProcessing')
+
+plt.legend()
+
+plt.show()
